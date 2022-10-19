78a4aab7ff1cf76df3020ceb177620f6452f7b72
ryanAbukarn <ryan.code891@gmail.com>
2022-10-19 13:27:13 -0400

    last commit

--- VDL/__init__.py
+++ VDL/__init__.py
@@ -14,10 +14,9 @@ DBNAME = os.getenv("DB_NAME")
 USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("DB_PASSWORD")
 sslmode = os.getenv("sslmode")
-print(os.environ)
 conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(HOST, USER, DBNAME, PASSWORD, sslmode)
 conn = psycopg2.connect(conn_string)
-print("Connection established")
+print("DB Connection established")
 
 cursor = conn.cursor(cursor_factory=RealDictCursor)
 
